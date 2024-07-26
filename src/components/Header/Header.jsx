6e372f050f785a70755487c447abd853bf36e6a8
Taylor Blythe <t65blythe@yahoo.com>
2024-07-25 21:07:32 -0500

    Fix: logo and avatar src value

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
 
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src="{logo}" />
+      <img className="header__logo" src={logo} />
       <p className="header__date-and-location">Date, Location</p>
       <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
-        <img src="{avatar}" alt="Terrence Tegegne" className="header__avatar" />
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
       </div>
     </header>
   );
