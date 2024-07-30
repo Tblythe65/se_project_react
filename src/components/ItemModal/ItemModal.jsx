8aed5558510848a8c70fd0be9b7d2d29f6485190
Taylor Blythe <t65blythe@yahoo.com>
2024-07-29 19:33:01 -0500

    Fix: modal opened

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,7 +2,7 @@ import "./ItemModal.css";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_open"}`}>
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button className="modal__close" type="button" onClick={onClose}>
           CLOSE
